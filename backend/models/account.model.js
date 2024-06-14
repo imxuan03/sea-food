e7aa122c06c4f4b7557724261d1caa8da8a5fb0d
imxuan03 <txuan0164@gmail.com>
2024-06-14 11:36:28 +0700

    3

--- backend/models/account.model.js
+++ backend/models/account.model.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose')
 
 const accountSchema = new mongoose.Schema({
     fullName:String, 
+    username: String, 
     email: String, 
     password: String,
     token: String,  
