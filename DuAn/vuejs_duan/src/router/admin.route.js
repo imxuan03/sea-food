8f9aefaeacc15f1bb1b8b588ea7d70c35feb3413
VThanhDat <121113462+VThanhDat@users.noreply.github.com>
2024-06-14 13:01:48 +0700

    fix admin route

--- DuAn/vuejs_duan/src/router/admin.route.js
+++ DuAn/vuejs_duan/src/router/admin.route.js
@@ -1,27 +1,28 @@
-// const requireAdminAuth = (to, from, next) => {
-//   try {
+const requireAdminAuth = (to, from, next) => {
+  try {
 
-//     const token = document.cookie.split(';').find(cookie => /\btoken\b/.test(cookie));
-//     if (token) {
-//       next();
-//     } else {
-//       next('/admin/auth/login');
-//     }
-//   } catch (error) {
-//     next('/admin/auth/login');
-//   }
-// };
+    const token = document.cookie.split(';').find(cookie => /\btoken\b/.test(cookie));
+    if (token) {
+      next();
+    } else {
+      next('/admin/auth/login');
+    }
+  } catch (error) {
+    next('/admin/auth/login');
+  }
+};
 
 const adminRoutes = [
   {
     path: '/admin/auth/login',
     name: 'login',
-    component: () => import('@/views/admin/pages/login/Login.vue')
+    component: () => import('@/views/admin/pages/login/Login.vue'),
   },
   {
     path: '/admin/dashboard',
     name: 'dashboard',
-    component: () => import('@/views/admin/pages/sea_foods/Dashboard.vue')
+    component: () => import('@/views/admin/pages/sea_foods/Dashboard.vue'),
+    beforeEnter: requireAdminAuth
   }
 ];
 
