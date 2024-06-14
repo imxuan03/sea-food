21acf7c822044d92d7084c2dd583cdf295a92fbc
imxuan03 <txuan0164@gmail.com>
2024-06-14 11:31:08 +0700

    2

--- backend/routes/admin/index.route.js
+++ backend/routes/admin/index.route.js
@@ -1,6 +1,13 @@
 const configSystem = require("../../config/system") 
 
+const authRouter = require("./auth.route")
+
 module.exports = (app) => {
   const ADMIN_PATH = '/' + configSystem.adminPrefix;
+
+  app.use(
+    ADMIN_PATH + "/auth", 
+    authRouter
+  );
 
 }
