f1ae6be019897037d02f02a7cd47db8fa76787f0
imxuan03 <txuan0164@gmail.com>
2024-06-14 10:39:06 +0700

    1

--- backend/routes/admin/index.route.js
+++ backend/routes/admin/index.route.js
@@ -1,28 +1,6 @@
 const configSystem = require("../../config/system") 
 
-const bookRouter = require('./book.route');
-const authRouter = require('./auth.route');
-const employeeRouter = require('./employee.route');
-const authMiddleware = require("../../middlewares/admin/auth.middleware");
-
 module.exports = (app) => {
   const ADMIN_PATH = '/' + configSystem.adminPrefix;
-
-  // app.use(
-  //   ADMIN_PATH + "/books",
-  //   authMiddleware.authRequire,
-  //   bookRouter
-  // );
-
-  // app.use(
-  //   ADMIN_PATH + "/auth", 
-  //   authRouter
-  // );
-
-  // app.use(
-  //   ADMIN_PATH + "/employee", 
-  //   authMiddleware.authRequire,
-  //   employeeRouter
-  // );
 
 }
